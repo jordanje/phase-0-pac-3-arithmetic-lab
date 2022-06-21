b8938dd470e851b9a85d6613b5231a903f5e7a84
Jordan Jennings <jordanjennings12@gmail.com>
2022-06-20 21:07:07 -0400

    edits

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-function add(){
-    
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a/b
+}
+
+function increment(n){
+    return ++n
+}
+
+function decrement(n){
+    return --n
+}
+
+function makeInt(n){
+  let parsedInt = parseInt(n, 10);
+  return parsedInt;
+}
+
+function preserveDecimal(n){
+    let parsedFloat = parseFloat(n);
+    return parsedFloat
 }
